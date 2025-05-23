8ed22e4bbd590edf4c278d5728aa9a1cf9ce96ac
w-maks <wmaksymiak@student.agh.edu.pl>
2025-05-24 01:02:42 +0200

    fixes

--- solver.cpp
+++ solver.cpp
@@ -132,12 +132,13 @@ void PoissonSolver::runParabolic(const int iterations) {
 
 void PoissonSolver::runGradient(int iterations, const std::vector<double>& betas, double delta) {
     for (const double beta : betas) {
+        u = Grid(2 * N + 1, 2 * N + 1, 0.0);
         std::ofstream sFile("S(iteration)[beta="+ std::to_string(beta)+ "]" + addon + ".csv");
         sFile << "iteration,S\n";
-        const double S0 = S();
-        sFile << 0 << ',' << S0 << '\n';
+        sFile << 0 << ',' << S() << '\n';
 
         for (int iter = 1; iter <= iterations; ++iter) {
+            const double S0 = S();
             for (int i = 1; i < 2 * N; ++i) {
                 for (int j = 1; j < 2 * N; ++j) {
                     const double Sloc0 = Sloc(i,j, 0.0);
